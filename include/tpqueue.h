825bd81d2b55da198b8b719b9f97aa7f541fa23f
KirillKochenov <124993480+KirillKochenov@users.noreply.github.com>
2023-04-24 22:14:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,35 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
+#include <list>
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    std::list <T> Queue;
+
+ public:
+    void push(const T& tp) {
+        if (Queue.empty()) {
+            Queue.push_back(tp);
+        } else {
+            bool flag = false;
+            for (auto step = Queue.begin(); step != Queue.end(); step++) {
+                if ((* step).prior < tp.prior) {
+                    Queue.insert(step, tp);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                Queue.push_back(tp);
+            }
+        }
+    }
+    T pop() {
+        T q = Queue.front();
+        Queue.pop_front();
+        return q;
+    }
 };
 
 struct SYM {
